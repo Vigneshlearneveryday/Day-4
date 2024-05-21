a93b2cfd0d943a85e42c3d5dc65e6f24a5548b98
Vigneshlearneveryday <nauticalvigneshmenon@gmail.com>
2024-05-22 02:01:58 +0530

    update

--- AnonymousFunc/Rotate-array-k times.js
+++ AnonymousFunc/Rotate-array-k times.js
@@ -9,8 +9,3 @@ let rotate_AF = function(array,t){
 }
 rotate_AF(array1,times);
 
-//IIFE
-let array2 = [1,2,3,4,5,6,7,8];
-let rotate_IIFE = ( (array,t) => {
-    console.log(array.concat(array.splice(0,t)));
-})(array2,times);

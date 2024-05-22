6af19301f0b824616f899a7a47ddc1001c5b421d
Vigneshlearneveryday <nauticalvigneshmenon@gmail.com>
2024-05-22 14:15:09 +0530

    updated

---
+++ Task 1/IIFE/Printodd.js
@@ -0,0 +1,9 @@
+
+let a=[0,1,2,3,4,5,6,7,8,9];
+
+(function ()  {
+
+  let odd= a.filter(num => num %2 ==1);
+return odd;
+
+  })();
---
+++ Task 1/IIFE/sum-of-all-numbers.js
@@ -0,0 +1,8 @@
+(function () {
+    let sum = [1, 2, 3, 4].reduce(add, 5);
+    function add(accumulator, a) {
+      return accumulator + a;
+    }
+    console.log(sum);
+  })()
+  
